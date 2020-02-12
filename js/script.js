ceb8362dbdbbbfb398f434546eab5ca463597b1d
YanaBrewster <yana.brewster@gmail.com>
2020-02-12 14:28:25 +1300

    mockaroo API

--- js/script.js
+++ js/script.js
@@ -1,73 +1,12 @@
 // console.log("js");
 
-// to test jquery
  $(document).ready(function(){
-   $('body').click(function(){
-    $(this).css('color','blue');
-   });
 
-
-
-// // JS AARAY OF DATA
-//
-// var jsData = [
-//   {
-//     name: "Peter",
-//     age: 50
-//   },
-//   {
-//     name: "Anna",
-//     age: 24
-//   }
-// ];
-//
-// // DISPLAY JS OBJECTS IN THE BROWSER WINDOW
-//
-// var i;
-// for (var i = 0; i < jsData.length; i++) {
-// document.getElementById('result').innerHTML +=
-// "<h2>" + jsData[i].name + " is "  + jsData[i].age + "</h2>"
-// };
-//
-//
-// // JSON DATA
-//
-// var jsonData = [
-//   {
-//     "name" : "Laura",
-//     "age" : 50
-//   },
-//   {
-//     "name" : "Tim",
-//     "age" : 24
-//   }
-// ];
-//
-// // DISPLAY JSON OBJECTS IN THE BROWSER WINDOW
-//
-// var j;
-// for (var j = 0; j < jsonData.length; j++) {
-// document.getElementById('result').innerHTML +=
-// "<h2>" + jsonData[j].name + " is "  + jsonData[j].age + "</h2>"
-// };
-//
-// // JSON FROM EXTERNAL FILE
-//
-// var person = JSON.parse(myData);
-// console.log(person);
-//
-// // DISPLAY JSON OBJECTS IN THE BROWSER WINDOW
-//
-// var k;
-// for (var k = 0; k < person.length; k++) {
-// document.getElementById('result').innerHTML +=
-// "<h2>" + person[k].name + " is "  + person[k].age + "</h2>"
-// };
 
 // AJAX METHOD
 
 $.ajax({
-  url:'js/mock_data.json',
+  url:'https://my.api.mockaroo.com/plant.json?key=d0ea6160',
   type:'GET',
   data:'json',
   success: function(data){
@@ -75,13 +14,13 @@ $.ajax({
     var i;
     for (i=0; i<data.length; i++){
       document.getElementById('result').innerHTML +=
-      '<div class="col col-sm-6 col-md-4 col-lg-4 border border-secondary rounded mx-1 my-1">' +
-      '<h1>' + data[i].first_name  + ' ' +
-       data[i].last_name + '</h1>' +
-       '<h2>' + data[i].gender + '</h2>' +
-       '<h3>' + data[i].email + '<h3>' +
-       '<img class="img-thumbnail" src="' + data[i].avatar + '" alt="Avatar">' +
-      '</div>';
+        '<div class="col">'+
+        '<div class="card" style="width: 18rem;">' +
+        '<img src="'+ data[i].image +'" class="card-img-top" alt="Plant Image">' +
+        '<div class="card-body"><h5 class="card-title">' + data[i].plant_name + '</h5>' +
+        '<p class="card-text">Plant Family: '+ data[i].scientific_name + '</p>' +
+        '<p class="card-text">Plant Family: '+ data[i].plant_family + '</p>' +
+        '<a href="#" class="btn btn-info">More Info</a></div></div> '
     }
 
   },//success
